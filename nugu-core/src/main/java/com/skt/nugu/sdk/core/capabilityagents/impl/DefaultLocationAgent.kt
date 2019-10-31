58c5dc4551464db409e61ad0810bc6b678fea894
sangho.lee <sangho0907.lee@sk.com>
2019-10-31 13:58:05 +0900

    Implement location agent (#10)

---
+++ nugu-core/src/main/java/com/skt/nugu/sdk/core/capabilityagents/impl/DefaultLocationAgent.kt
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2019 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.skt.nugu.sdk.core.capabilityagents.impl
+
+import com.google.gson.JsonObject
+import com.skt.nugu.sdk.core.interfaces.capability.location.AbstractLocationAgent
+import com.skt.nugu.sdk.core.interfaces.capability.location.LocationAgentFactory
+import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
+import com.skt.nugu.sdk.core.interfaces.context.ContextSetterInterface
+import com.skt.nugu.sdk.core.interfaces.context.StateRefreshPolicy
+import com.skt.nugu.sdk.core.interfaces.location.LocationProvider
+
+object DefaultLocationAgent {
+    private const val TAG = "DefaultLocationAgent"
+
+    val FACTORY = object : LocationAgentFactory {
+        override fun create(): AbstractLocationAgent = Impl()
+    }
+
+    internal class Impl : AbstractLocationAgent() {
+        private var locationProvider: LocationProvider? = null
+
+        override val namespaceAndName: NamespaceAndName =
+            NamespaceAndName("supportedInterfaces", NAMESPACE)
+
+        override fun setLocationProvider(provider: LocationProvider) {
+            locationProvider = provider
+        }
+
+        override fun provideState(
+            contextSetter: ContextSetterInterface,
+            namespaceAndName: NamespaceAndName,
+            stateRequestToken: Int
+        ) {
+            contextSetter.setState(
+                namespaceAndName,
+                buildContext(),
+                StateRefreshPolicy.ALWAYS,
+                stateRequestToken
+            )
+        }
+
+        private fun buildContext(): String = JsonObject().apply {
+            addProperty(
+                "version",
+                VERSION
+            )
+            val location = locationProvider?.getLocation()
+            if (location != null) {
+                add("current", JsonObject().apply {
+                    addProperty("latitude", location.latitude)
+                    addProperty("longitude", location.longitude)
+                })
+            }
+        }.toString()
+    }
+}
