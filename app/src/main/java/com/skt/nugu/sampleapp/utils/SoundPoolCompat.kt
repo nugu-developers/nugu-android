d0fc39eb2d3db8168c5afb9c44cddff7da40fbf6
bj.lee <tommy500@sk.com>
2019-12-30 17:29:21 +0900

    Implement local sound playback for common error handling #151

--- app/src/main/java/com/skt/nugu/sampleapp/utils/SoundPoolCompat.kt
+++ app/src/main/java/com/skt/nugu/sampleapp/utils/SoundPoolCompat.kt
@@ -26,8 +26,12 @@ import com.skt.nugu.sampleapp.R
 
 object SoundPoolCompat {
     private var soundPool: SoundPool? = null
-    private val soundIds = ConcurrentHashMap<LocalBeep, Int>()
+    private val localBeepIds = ConcurrentHashMap<LocalBeep, Int>()
+    private val localTTSIds = ConcurrentHashMap<LocalTTS, Int>()
 
+    /**
+     * The enum LocalBeep describes the state of asr.
+     **/
     enum class LocalBeep constructor(resId: Int) {
         FAIL(R.raw.responsefail_800ms),
         SUCCESS(R.raw.responsesuccess_800ms),
@@ -41,9 +45,29 @@ object SoundPoolCompat {
         }
     }
 
+    /**
+     * The enum LocalTTS describes the state of error.
+     * @see [https://developers-doc.nugu.co.kr/nugu-sdk/sdk-design-guide/error-handling]
+     **/
+    enum class LocalTTS constructor(resId: Int) {
+        DEVICE_GATEWAY_NETWORK_ERROR(R.raw.device_gw_error_001),
+        DEVICE_GATEWAY_SERVER_ERROR_TRY_AGAIN(R.raw.device_gw_error_002),
+        DEVICE_GATEWAY_UNAUTHORIZED_ERROR(R.raw.device_gw_error_003),
+        DEVICE_GATEWAY_REQUEST_TIMEOUT_ERROR(R.raw.device_gw_error_004),
+        DEVICE_GATEWAY_NOTACCEPTABLE_ERROR(R.raw.device_gw_error_005),
+        DEVICE_GATEWAY_TTS_ERROR(R.raw.device_gw_error_006),
+        DEVICE_GATEWAY_PLAY_ROUTER_ERROR(R.raw.device_gw_error_006);
+
+        var resourceId: Int = 0
+            internal set
+
+        init {
+            this.resourceId = resId
+        }
+    }
 
     /**
-     * Load the sound from the specified raw files.
+     * Load the resource from the specified raw files.
      */
     fun load(context: Context) {
         release()
@@ -59,16 +83,30 @@ object SoundPoolCompat {
 
         LocalBeep.values().forEach {
             soundPool?.let { pool ->
-                soundIds[it] = pool.load(context, it.resourceId, 1)
+                localBeepIds[it] = pool.load(context, it.resourceId, 1)
+            }
+        }
+        LocalTTS.values().forEach {
+            soundPool?.let { pool ->
+                localTTSIds[it] = pool.load(context, it.resourceId, 1)
             }
         }
     }
 
     /**
-     * Play a sound from a sound ID.
+     * Play a beep from a sound ID.
      */
     fun play(beep: LocalBeep) {
-        soundIds[beep]?.let {
+        localBeepIds[beep]?.let {
+            soundPool?.play(it, 1f, 1f, 0, 0, 1f)
+        }
+    }
+
+    /**
+     * Play a tts from a sound ID.
+     */
+    fun play(tts: LocalTTS) {
+        localTTSIds[tts]?.let {
             soundPool?.play(it, 1f, 1f, 0, 0, 1f)
         }
     }
@@ -78,7 +116,8 @@ object SoundPoolCompat {
      */
     fun release() {
         Log.d("SoundPoolCompat", "release")
-        soundIds.clear()
+        localBeepIds.clear()
+        localTTSIds.clear()
         soundPool?.release()
     }
 }
