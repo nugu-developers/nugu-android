01d75e97f60ccd225d581be8b945bc410af5c196
sangho.lee <sangho0907.lee@sk.com>
2020-02-01 11:30:50 +0900

    Apply battery charing status (#238)
    * add/implement isCharing API
    * update isCharing context field

--- nugu-android-helper/src/main/java/com/skt/nugu/sdk/platform/android/battery/AndroidBatteryStatusProvider.kt
+++ nugu-android-helper/src/main/java/com/skt/nugu/sdk/platform/android/battery/AndroidBatteryStatusProvider.kt
@@ -19,19 +19,56 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
+import android.os.SystemClock
 import com.skt.nugu.sdk.agent.system.BatteryStatusProvider
+import com.skt.nugu.sdk.core.utils.Logger
 
 /**
  * Default implementation of [BatteryStatusProvider] for Android Battery status.
  */
 class AndroidBatteryStatusProvider(
     private val context: Context
-) : BatteryStatusProvider{
+) : BatteryStatusProvider {
+    companion object {
+        private const val TAG = "AndroidBatteryStatusProvider"
+        private const val REFRESH_INTERVAL = 10L
+    }
+
+    private var lastUsedBatteryStatusIntentCreated: Long = -1L
+    private var lastUsedBatteryStatusIntent: Intent? = null
 
     override fun getBatteryLevel(): Int {
-        context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))?.apply {
-            return getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+        val level = getBatteryStatusIntent()?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        Logger.d(TAG, "[getBatteryLevel] level: $level")
+        return level
+    }
+
+    override fun isCharging(): Boolean? {
+        var pluggedStatus: Int? = null
+        val charging = getBatteryStatusIntent()?.let {
+            pluggedStatus = it.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
+            when (pluggedStatus) {
+                BatteryManager.BATTERY_PLUGGED_AC,
+                BatteryManager.BATTERY_PLUGGED_USB,
+                BatteryManager.BATTERY_PLUGGED_WIRELESS -> true
+                else -> false
+            }
         }
-        return -1
+        Logger.d(TAG, "[isCharging] charging: $charging / pluggedStatus: $pluggedStatus")
+        return charging
+    }
+
+    private fun getBatteryStatusIntent(): Intent? {
+        val currentElapsed = SystemClock.elapsedRealtime()
+        var intent = lastUsedBatteryStatusIntent
+
+        if(intent == null || currentElapsed - lastUsedBatteryStatusIntentCreated > REFRESH_INTERVAL) {
+            intent = context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            lastUsedBatteryStatusIntent = intent
+            lastUsedBatteryStatusIntentCreated = currentElapsed
+            Logger.d(TAG, "[getBatteryStatusIntent] battery status refreshed")
+        }
+
+        return intent
     }
 }
