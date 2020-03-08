aaea3c71643e74ef0a8e06b104c41f60ace36156
sangho.lee <sangho0907.lee@sk.com>
2020-03-08 11:18:45 +0900

    Support RequestPlayCommand directives (#371)

---
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/audioplayer/playback/AudioPlayerRequestPlayCommandDirectiveHandler.kt
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2020 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.agent.audioplayer.playback
+
+import com.skt.nugu.sdk.agent.AbstractDirectiveHandler
+import com.skt.nugu.sdk.agent.DefaultAudioPlayerAgent
+import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
+import com.skt.nugu.sdk.core.interfaces.context.ContextGetterInterface
+import com.skt.nugu.sdk.core.interfaces.context.ContextRequester
+import com.skt.nugu.sdk.core.interfaces.directive.BlockingPolicy
+import com.skt.nugu.sdk.core.interfaces.message.MessageSender
+import com.skt.nugu.sdk.core.interfaces.message.request.EventMessageRequest
+
+class AudioPlayerRequestPlayCommandDirectiveHandler(
+    private val messageSender: MessageSender,
+    private val contextGetter: ContextGetterInterface
+): AbstractDirectiveHandler() {
+    companion object {
+        const val NAMESPACE =
+            DefaultAudioPlayerAgent.NAMESPACE
+        const val VERSION =
+            DefaultAudioPlayerAgent.VERSION
+
+        // v1.2
+        private const val NAME_REQUEST_PLAY_COMMAND = "RequestPlayCommand"
+        private const val NAME_REQUEST_PLAY_COMMAND_ISSUED = "${NAME_REQUEST_PLAY_COMMAND}Issued"
+        private val REQUEST_PLAY_COMMAND = NamespaceAndName(NAMESPACE, NAME_REQUEST_PLAY_COMMAND)
+    }
+
+    override fun preHandleDirective(info: DirectiveInfo) {
+        // no-op
+    }
+
+    override fun handleDirective(info: DirectiveInfo) {
+        info.result.setCompleted()
+        removeDirective(info.directive.getMessageId())
+        contextGetter.getContext(object: ContextRequester {
+            override fun onContextAvailable(jsonContext: String) {
+                val header = info.directive.header
+                val payload = info.directive.payload
+
+                val message = EventMessageRequest.Builder(jsonContext, header.namespace, NAME_REQUEST_PLAY_COMMAND_ISSUED, VERSION)
+                    .payload(payload)
+                    .build()
+
+                messageSender.sendMessage(message)
+            }
+
+            override fun onContextFailure(error: ContextRequester.ContextRequestError) {
+            }
+        }, NamespaceAndName("supportedInterfaces", NAMESPACE))
+    }
+
+    override fun cancelDirective(info: DirectiveInfo) {
+        removeDirective(info.directive.getMessageId())
+    }
+
+    override fun getConfiguration(): Map<NamespaceAndName, BlockingPolicy> {
+        val configurations = HashMap<NamespaceAndName, BlockingPolicy>()
+        val nonBlockingPolicy = BlockingPolicy()
+
+        configurations[REQUEST_PLAY_COMMAND] = nonBlockingPolicy
+
+        return configurations
+    }
+}
