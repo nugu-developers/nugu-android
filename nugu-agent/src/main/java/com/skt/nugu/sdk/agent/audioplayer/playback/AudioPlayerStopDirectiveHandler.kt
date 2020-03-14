853ce9b0ab2dc59f187b41bc0e6f1e88f795c70e
sangho.lee <sangho0907.lee@sk.com>
2020-03-14 11:25:12 +0900

    Split stop directive handler (#437)
    and integrate as playback directive handler.

--- nugu-agent/src/main/java/com/skt/nugu/sdk/agent/audioplayer/playback/AudioPlayerStopDirectiveHandler.kt
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/audioplayer/playback/AudioPlayerStopDirectiveHandler.kt
@@ -22,19 +22,19 @@ import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
 import com.skt.nugu.sdk.core.interfaces.directive.BlockingPolicy
 import com.skt.nugu.sdk.core.interfaces.directive.DirectiveGroupProcessorInterface
 import com.skt.nugu.sdk.core.interfaces.message.Directive
-import com.skt.nugu.sdk.core.utils.Logger
 
-class AudioPlayerPauseDirectiveHandler(
+class AudioPlayerStopDirectiveHandler(
     private val controller: Controller
 ) : AbstractDirectiveHandler(), DirectiveGroupProcessorInterface.Listener {
     companion object {
-        private const val NAME_PAUSE = "Pause"
-        private val PAUSE = NamespaceAndName(DefaultAudioPlayerAgent.NAMESPACE, NAME_PAUSE)
+        private const val NAME_STOP = "Stop"
+        private val STOP = NamespaceAndName(DefaultAudioPlayerAgent.NAMESPACE, NAME_STOP)
     }
+
     interface Controller {
-        fun onReceivePause(directive: Directive)
-        fun onExecutePause(directive: Directive)
-        fun onCancelPause(directive: Directive)
+        fun onReceive(directive: Directive)
+        fun onExecute(directive: Directive)
+        fun onCancel(directive: Directive)
     }
 
     override fun preHandleDirective(info: DirectiveInfo) {
@@ -44,29 +44,31 @@ class AudioPlayerPauseDirectiveHandler(
     override fun handleDirective(info: DirectiveInfo) {
         removeDirective(info.directive.getMessageId())
         info.result.setCompleted()
-        controller.onExecutePause(info.directive)
+        controller.onExecute(info.directive)
     }
 
     override fun cancelDirective(info: DirectiveInfo) {
         removeDirective(info.directive.getMessageId())
-        controller.onCancelPause(info.directive)
+        controller.onCancel(info.directive)
     }
 
-    override fun getConfiguration(): Map<NamespaceAndName, BlockingPolicy> = HashMap<NamespaceAndName, BlockingPolicy>().apply {
-        put(
-            PAUSE , BlockingPolicy(
-            BlockingPolicy.MEDIUM_AUDIO,
-            false
-        ))
-    }
+    override fun getConfiguration(): Map<NamespaceAndName, BlockingPolicy> =
+        HashMap<NamespaceAndName, BlockingPolicy>().apply {
+            put(
+                STOP, BlockingPolicy(
+                    BlockingPolicy.MEDIUM_AUDIO,
+                    false
+                )
+            )
+        }
 
     override fun onReceiveDirectives(directives: List<Directive>) {
         val directive = directives.firstOrNull() {
-            it.getNamespaceAndName() == PAUSE
+            it.getNamespaceAndName() == STOP
         }
 
         directive?.let {
-            controller.onReceivePause(it)
+            controller.onReceive(it)
         }
     }
 }
