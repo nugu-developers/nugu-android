237a3854ee98be7ab72b410401db3af529a2cfd7
sangho.lee <sangho0907.lee@sk.com>
2020-04-10 13:54:42 +0900

    Apply request command failed event (#588)

--- nugu-agent/src/main/java/com/skt/nugu/sdk/agent/audioplayer/AudioPlayerPlaybackInfoProvider.kt
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/audioplayer/AudioPlayerPlaybackInfoProvider.kt
@@ -1,6 +1,7 @@
 package com.skt.nugu.sdk.agent.audioplayer
 
 interface AudioPlayerPlaybackInfoProvider {
+    fun getCurrentState(): AudioPlayerAgentInterface.State
     fun getToken(): String?
     fun getOffsetInMilliseconds(): Long?
     fun getPlayServiceId(): String?
