d2bac758e385367545f8a4450d88d58f4cd3f6e5
shshshl09 <53801377+shshshl09@users.noreply.github.com>
2020-04-13 16:15:51 +0900

    Revert "Apply request command failed event (#588)"
    This reverts commit 237a3854ee98be7ab72b410401db3af529a2cfd7.

--- nugu-agent/src/main/java/com/skt/nugu/sdk/agent/DefaultAudioPlayerAgent.kt
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/DefaultAudioPlayerAgent.kt
@@ -188,8 +188,6 @@ class DefaultAudioPlayerAgent(
     }
 
     private val playbackInfoProvider = object : AudioPlayerPlaybackInfoProvider {
-        override fun getCurrentState(): AudioPlayerAgentInterface.State = currentActivity
-
         override fun getToken(): String? {
             val current = currentItem ?: return null
             return current.payload.audioItem.stream.token
