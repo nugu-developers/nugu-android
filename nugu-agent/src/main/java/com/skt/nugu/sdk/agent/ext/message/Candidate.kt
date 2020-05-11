709324d32219afeb776537ab8e869fbd20261fec
sangho.lee <sangho0907.lee@sk.com>
2020-05-11 13:56:50 +0900

    Prototype Message Interface v1.0 (#696)
    Provide context for Message Interface v1.0

---
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/ext/message/Candidate.kt
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2020 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.agent.ext.message
+
+data class Candidate(
+    val name: String,
+    val type: Type,
+    val number: String?,
+    val profileImgUrl: String?,
+    val message: String?,
+    val time: String?,
+    val score: String?
+) {
+    enum class Type {
+        CONTACT,
+        EXCHANGE,
+        T114,
+        NONE
+    }
+}
