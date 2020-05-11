0ff78a45340983009b5a74214197dce3b19b33b7
sangho.lee <sangho0907.lee@sk.com>
2020-05-11 13:56:50 +0900

    Prototype Message Interface v1.0 (#696)
    Provide context for Message Interface v1.0

---
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/ext/message/MessageAgent.kt
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2020 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.agent.ext.message
+
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import com.skt.nugu.sdk.agent.version.Version
+import com.skt.nugu.sdk.core.interfaces.capability.CapabilityAgent
+import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
+import com.skt.nugu.sdk.core.interfaces.context.ContextSetterInterface
+import com.skt.nugu.sdk.core.interfaces.context.ContextStateProviderRegistry
+import com.skt.nugu.sdk.core.interfaces.context.StateRefreshPolicy
+import com.skt.nugu.sdk.core.interfaces.context.SupportedInterfaceContextProvider
+import java.util.concurrent.Executors
+
+class MessageAgent(
+    private val client: MessageClient,
+    contextStateProviderRegistry: ContextStateProviderRegistry
+)
+    : CapabilityAgent
+    , SupportedInterfaceContextProvider {
+    companion object {
+        private const val TAG = "MessageAgent"
+
+        val VERSION = Version(1,0)
+    }
+
+    override fun getInterfaceName(): String = "Message"
+
+    private val executor = Executors.newSingleThreadExecutor()
+    private var currentContext: Context? = null
+
+    init {
+        contextStateProviderRegistry.setStateProvider(namespaceAndName, this, buildCompactContext().toString())
+    }
+
+    private fun buildCompactContext(): JsonObject = JsonObject().apply {
+        addProperty("version", VERSION.toString())
+    }
+
+    override fun provideState(
+        contextSetter: ContextSetterInterface,
+        namespaceAndName: NamespaceAndName,
+        stateRequestToken: Int
+    ) {
+        executor.submit {
+            val context = client.getContext()
+            if(currentContext != context) {
+                currentContext = context
+                    contextSetter.setState(namespaceAndName, buildCompactContext().apply {
+                        context.candidates?.let {
+                            add("candidates", JsonArray().apply {
+                                it.forEach {
+                                    add(JsonObject().apply {
+                                        addProperty("name", it.name)
+                                        addProperty("type", it.type.name)
+                                        it.number?.let {
+                                            addProperty("number", it)
+                                        }
+                                        it.profileImgUrl?.let {
+                                            addProperty("profileImgUrl", it)
+                                        }
+                                        it.message?.let {
+                                            addProperty("message", it)
+                                        }
+                                        it.time?.let {
+                                            addProperty("time", it)
+                                        }
+                                        it.score?.let {
+                                            addProperty("score", it)
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    }.toString(), StateRefreshPolicy.ALWAYS, stateRequestToken)
+            } else {
+                contextSetter.setState(namespaceAndName, null, StateRefreshPolicy.ALWAYS, stateRequestToken)
+            }
+        }
+    }
+}
