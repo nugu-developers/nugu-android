98e0e4062adbf32ff9b9975f548ad6ab2bb741e7
sangho.lee <sangho0907.lee@sk.com>
2020-05-15 13:59:07 +0900

    Support EndCall directive and event (#698)

---
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/ext/phonecall/handler/EndCallDirectiveHandler.kt
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2020 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.agent.ext.phonecall.handler
+
+import com.skt.nugu.sdk.agent.AbstractDirectiveHandler
+import com.skt.nugu.sdk.agent.ext.phonecall.PhoneCallAgent
+import com.skt.nugu.sdk.agent.ext.phonecall.payload.EndCallPayload
+import com.skt.nugu.sdk.agent.util.MessageFactory
+import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
+import com.skt.nugu.sdk.core.interfaces.directive.BlockingPolicy
+
+class EndCallDirectiveHandler (
+    private val controller: Controller
+) : AbstractDirectiveHandler() {
+    companion object {
+        private const val NAME_END_CALL = "EndCall"
+
+        private val END_CALL = NamespaceAndName(PhoneCallAgent.NAMESPACE, NAME_END_CALL)
+    }
+
+    interface Controller {
+        fun endCall(payload: EndCallPayload)
+    }
+
+    override fun preHandleDirective(info: DirectiveInfo) {
+    }
+
+    override fun handleDirective(info: DirectiveInfo) {
+        removeDirective(info.directive.getMessageId())
+
+        val payload =
+            MessageFactory.create(info.directive.payload, EndCallPayload::class.java)
+        if (payload == null) {
+            info.result.setFailed("Invalid Payload")
+        } else {
+            info.result.setCompleted()
+            controller.endCall(payload)
+        }
+    }
+
+    override fun cancelDirective(info: DirectiveInfo) {
+        removeDirective(info.directive.getMessageId())
+    }
+
+    override fun getConfiguration(): Map<NamespaceAndName, BlockingPolicy> {
+        val configurations = HashMap<NamespaceAndName, BlockingPolicy>()
+
+        configurations[END_CALL] = BlockingPolicy()
+
+        return configurations
+    }
+}
