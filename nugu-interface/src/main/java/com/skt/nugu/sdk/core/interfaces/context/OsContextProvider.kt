d9dd077cf73c28511a8920ce3cf4c021d5e0d620
sangho.lee <sangho0907.lee@sk.com>
2020-05-29 10:10:53 +0900

    Apply client's os context (#747)
    * Create OsContextProvider
    * Setup Android OS context

---
+++ nugu-interface/src/main/java/com/skt/nugu/sdk/core/interfaces/context/OsContextProvider.kt
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2020 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.core.interfaces.context
+
+import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
+
+abstract class OsContextProvider: ClientContextProvider {
+    enum class Type(val value: String) {
+        ANDROID("Android"),
+        IOS("iOS"),
+        LINUX("Linux")
+    }
+
+    final override fun getName(): String = "os"
+
+    final override fun provideState(
+        contextSetter: ContextSetterInterface,
+        namespaceAndName: NamespaceAndName,
+        stateRequestToken: Int
+    ) {
+        contextSetter.setState(
+            namespaceAndName,
+            "\"${getType().value}\"",
+            StateRefreshPolicy.NEVER,
+            stateRequestToken
+        )
+    }
+
+    abstract fun getType(): Type
+}
