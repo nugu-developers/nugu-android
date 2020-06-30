2cf498c55b691778c68aa2e0d03dc1645092fb1e
sangho.lee <sangho0907.lee@sk.com>
2020-06-30 14:13:02 +0900

    Add WakeupWordContextProvider class (#867)

---
+++ nugu-interface/src/main/java/com/skt/nugu/sdk/core/interfaces/context/WakeupWordContextProvider.kt
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2020 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.core.interfaces.context
+
+import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
+
+abstract class WakeupWordContextProvider : ClientContextProvider {
+    override fun getName(): String = "wakeupWord"
+
+    override fun provideState(
+        contextSetter: ContextSetterInterface,
+        namespaceAndName: NamespaceAndName,
+        stateRequestToken: Int
+    ) {
+        contextSetter.setState(
+            namespaceAndName,
+            object: ClientContextState {
+                override fun toFullJsonString(): String = "\"${getWakeupWord()}\""
+            },
+            StateRefreshPolicy.ALWAYS,
+            stateRequestToken
+        )
+    }
+
+    abstract fun getWakeupWord(): String
+}
