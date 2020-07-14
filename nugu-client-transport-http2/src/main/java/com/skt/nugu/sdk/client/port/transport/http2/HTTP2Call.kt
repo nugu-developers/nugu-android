87b3454b095ab843a1d9107016342e8e0ce6d164
bj.lee <tommy500@sk.com>
2020-07-14 18:48:03 +0900

    Allow nullable callback (#925)

--- nugu-client-transport-http2/src/main/java/com/skt/nugu/sdk/client/port/transport/http2/HTTP2Call.kt
+++ nugu-client-transport-http2/src/main/java/com/skt/nugu/sdk/client/port/transport/http2/HTTP2Call.kt
@@ -43,7 +43,7 @@ internal class HTTP2Call(
 
     override fun request() = request
 
-    override fun enqueue(callback: MessageSender.Callback) {
+    override fun enqueue(callback: MessageSender.Callback?) {
         this.callback = callback
 
         synchronized(this) {
