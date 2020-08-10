bd2a2359e273a9a312086098fa6f1ae8f0b99ca9
sangho.lee <sangho0907.lee@sk.com>
2020-08-10 14:32:30 +0900

    Add classes for Display Layer Policy #998

---
+++ nugu-interface/src/main/java/com/skt/nugu/sdk/core/interfaces/display/InterLayerDisplayPolicyManager.kt
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2020 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.core.interfaces.display
+
+interface InterLayerDisplayPolicyManager {
+    interface Layer {
+        fun getLayerType(): LayerType
+        fun getDialogRequestId(): String
+    }
+
+    interface DisplayLayer: Layer {
+        fun getPlayServiceId(): String?
+        fun clear()
+    }
+
+    interface PlayLayer: Layer {
+        fun getPlayServiceId(): String?
+        fun getPushPlayServiceId(): String?
+    }
+
+    fun onDisplayLayerRendered(layer: DisplayLayer)
+    fun onDisplayLayerCleared(layer: DisplayLayer)
+    fun onPlayStarted(layer: PlayLayer)
+}
