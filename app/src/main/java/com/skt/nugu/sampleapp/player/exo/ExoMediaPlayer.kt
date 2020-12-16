33bed3e789f1d7a9e8405690c87a044cd35a10ee
SKT.한유선 <hanyuseon@sk.com>
2020-12-16 15:41:11 +0900

    Revert fixing exoPlayer
    - it already fixed by previous other commit

--- app/src/main/java/com/skt/nugu/sampleapp/player/exo/ExoMediaPlayer.kt
+++ app/src/main/java/com/skt/nugu/sampleapp/player/exo/ExoMediaPlayer.kt
@@ -147,7 +147,7 @@ class ExoMediaPlayer(
                 Player.STATE_BUFFERING -> eventNotifier.notifyBufferUnderRun(sourceId)
                 Player.STATE_READY -> {
                     if (durationCallSourceId.id < sourceId.id) {
-                        durationCallSourceId.id = sourceId.id
+                        durationCallSourceId = sourceId
                         durationListener?.onRetrieved(sourceId, getDuration(sourceId))
                     }
                     eventNotifier.notifyBufferRefilled(sourceId)
