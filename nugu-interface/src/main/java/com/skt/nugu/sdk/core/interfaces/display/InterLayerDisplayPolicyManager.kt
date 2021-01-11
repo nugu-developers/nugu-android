bb8af71e6b83307e11530d6436e249f8189c0a3a
sangho.lee <sangho0907.lee@sk.com>
2021-01-11 18:10:36 +0900

    Write document #1509

--- nugu-interface/src/main/java/com/skt/nugu/sdk/core/interfaces/display/InterLayerDisplayPolicyManager.kt
+++ nugu-interface/src/main/java/com/skt/nugu/sdk/core/interfaces/display/InterLayerDisplayPolicyManager.kt
@@ -16,30 +16,91 @@
 
 package com.skt.nugu.sdk.core.interfaces.display
 
+/**
+ * Manage a display's lifecycle between layers.
+ */
 interface InterLayerDisplayPolicyManager {
+    /**
+     * Base interface for Layer
+     */
     interface Layer {
         fun getLayerType(): LayerType
         fun getDialogRequestId(): String
         fun getPushPlayServiceId(): String?
     }
 
+    /**
+     * The layer for display
+     */
     interface DisplayLayer: Layer {
+        /**
+         * clear the display
+         */
         fun clear()
+
+        /**
+         * refresh the display's timer
+         */
         fun refresh()
     }
 
+    /**
+     * The layer for play
+     */
     interface PlayLayer: Layer
 
+    /**
+     * Should be called when the display rendered
+     *
+     * @param layer the layer rendered
+     */
     fun onDisplayLayerRendered(layer: DisplayLayer)
+
+    /**
+     * Should be called when the display cleared
+     *
+     * @param layer the layer cleared
+     */
     fun onDisplayLayerCleared(layer: DisplayLayer)
+
+    /**
+     * Should be called when the play started
+     *
+     * @param layer the layer which play started
+     */
     fun onPlayStarted(layer: PlayLayer)
+
+    /**
+     * Should be called when the play finished
+     *
+     * @param layer the layer which play finished
+     */
     fun onPlayFinished(layer: PlayLayer)
 
+    /**
+     * The listener
+     */
     interface Listener {
+        /**
+         * Called when display layer rendered
+         */
         fun onDisplayLayerRendered(layer: DisplayLayer)
+
+        /**
+         * Called when display layer cleared
+         */
         fun onDisplayLayerCleared(layer: DisplayLayer)
     }
 
+    /**
+     * Add listener
+     * @param listener the listener
+     */
     fun addListener(listener: Listener)
+
+    /**
+     * Remove listener
+     * @param listener the listener
+     */
     fun removeListener(listener: Listener)
 }
