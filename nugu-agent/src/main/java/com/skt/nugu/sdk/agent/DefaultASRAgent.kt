98ce9794fff7640e60fa7c9b5547ee67ffa5239c
sangho.lee <sangho0907.lee@sk.com>
2021-03-04 15:33:19 +0900

    Update ASRAgent v1.5 #1613

--- nugu-agent/src/main/java/com/skt/nugu/sdk/agent/DefaultASRAgent.kt
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/DefaultASRAgent.kt
@@ -74,7 +74,7 @@ class DefaultASRAgent(
         private const val TAG = "DefaultASRAgent"
 
         const val NAMESPACE = "ASR"
-        val VERSION = Version(1,4)
+        val VERSION = Version(1,5)
 
         const val NAME_EXPECT_SPEECH = "ExpectSpeech"
         const val NAME_RECOGNIZE = "Recognize"
