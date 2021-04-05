cd5d5a4612986c88391c125aee36b0007aa06a39
sangho.lee <sangho0907.lee@sk.com>
2021-04-05 15:12:37 +0900

    Write test for LocationAgent #1703

--- nugu-agent/src/main/java/com/skt/nugu/sdk/agent/location/LocationAgent.kt
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/location/LocationAgent.kt
@@ -1,4 +1,20 @@
 /**
+ * Copyright (c) 2021 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Copyright (c) 2019 SK Telecom Co., Ltd. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,11 +29,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.skt.nugu.sdk.agent
+package com.skt.nugu.sdk.agent.location
 
 import com.google.gson.JsonObject
-import com.skt.nugu.sdk.agent.location.Location
-import com.skt.nugu.sdk.agent.location.LocationProvider
 import com.skt.nugu.sdk.agent.version.Version
 import com.skt.nugu.sdk.core.interfaces.capability.CapabilityAgent
 import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
@@ -26,8 +40,8 @@ import com.skt.nugu.sdk.core.utils.Logger
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
-class DefaultLocationAgent(
-    contextManager: ContextManagerInterface,
+class LocationAgent(
+    contextStateProviderRegistry: ContextStateProviderRegistry,
     private val locationProvider: LocationProvider
 ) : CapabilityAgent,
     SupportedInterfaceContextProvider {
@@ -36,13 +50,13 @@ class DefaultLocationAgent(
 
         const val NAMESPACE = "Location"
 
-        private val VERSION = Version(1, 0)
+        internal val VERSION = Version(1, 0)
     }
 
     private var contextUpdateLock = ReentrantLock()
 
     init {
-        contextManager.setStateProvider(namespaceAndName, this)
+        contextStateProviderRegistry.setStateProvider(namespaceAndName, this)
     }
 
     override fun getInterfaceName(): String = NAMESPACE
@@ -84,7 +98,7 @@ class DefaultLocationAgent(
             contextSetter.setState(
                 namespaceAndName,
                 if (contextType == ContextType.COMPACT) StateContext.CompactContextState else StateContext(
-                    locationProvider?.getLocation()
+                    locationProvider.getLocation()
                 ),
                 StateRefreshPolicy.ALWAYS,
                 contextType,
---
+++ nugu-agent/src/test/java/com/skt/nugu/sdk/agent/location/LocationAgentTest.kt
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2021 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.agent.location
+
+import com.google.gson.JsonParser
+import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
+import com.skt.nugu.sdk.core.interfaces.context.ContextManagerInterface
+import com.skt.nugu.sdk.core.interfaces.context.ContextSetterInterface
+import com.skt.nugu.sdk.core.interfaces.context.ContextType
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.kotlin.*
+import java.util.concurrent.Executors
+
+class LocationAgentTest {
+    @Test
+    fun testRegisteredAtContextManager() {
+        val contextManager: ContextManagerInterface = mock()
+        val agent = LocationAgent(contextManager, mock())
+        verify(contextManager).setStateProvider(agent.namespaceAndName, agent)
+    }
+
+    @Test
+    fun testGetInterfaceName() {
+        val agent = LocationAgent(mock(), mock())
+        Assert.assertTrue(agent.getInterfaceName() == LocationAgent.NAMESPACE)
+    }
+
+    @Test
+    fun testProvideStateWithCompactContext() {
+        val agent = LocationAgent(mock(), mock())
+        val contextSetter: ContextSetterInterface = mock()
+
+        val namespaceAndName = NamespaceAndName("", "")
+        val token = 1
+
+        agent.provideState(contextSetter, namespaceAndName, ContextType.COMPACT, token)
+
+        Executors.newSingleThreadExecutor().submit {
+            verify(contextSetter, times(1)).setState(
+                eq(namespaceAndName),
+                eq(LocationAgent.StateContext.CompactContextState),
+                any(),
+                eq(ContextType.COMPACT),
+                eq(token)
+            )
+        }.get()
+    }
+
+    @Test
+    fun testProvideStateWithFullContext() {
+        val provider: LocationProvider = mock()
+        whenever(provider.getLocation()).thenReturn(Location("10","20"))
+
+        val agent = LocationAgent(mock(), provider)
+        val contextSetter: ContextSetterInterface = mock()
+
+        val namespaceAndName = NamespaceAndName("", "")
+        val token = 1
+        val state = LocationAgent.StateContext(provider.getLocation())
+
+        agent.provideState(contextSetter, namespaceAndName, ContextType.FULL, token)
+
+        Executors.newSingleThreadExecutor().submit {
+            verify(contextSetter, atLeastOnce()).setState(
+                eq(namespaceAndName),
+                eq(state),
+                any(),
+                eq(ContextType.FULL),
+                eq(token)
+            )
+        }.get()
+    }
+
+    @Test
+    fun testStateContextForVersion() {
+        val json = JsonParser.parseString(LocationAgent.StateContext(null).value())
+        Assert.assertTrue(json.asJsonObject["version"].asString == LocationAgent.VERSION.toString())
+    }
+
+    @Test
+    fun testStateContextWithNullLocation() {
+        val json = JsonParser.parseString(LocationAgent.StateContext(null).value())
+        Assert.assertFalse(json.asJsonObject.has("current"))
+    }
+
+    @Test
+    fun testStateContextWithLocation() {
+        val json = JsonParser.parseString(LocationAgent.StateContext(Location("10","20")).value())
+        Assert.assertTrue(json.asJsonObject["current"].asJsonObject.has("latitude"))
+        Assert.assertTrue(json.asJsonObject["current"].asJsonObject.has("longitude"))
+    }
+}
