ba97c8f40998fa4c50120058c91c5b1ac4ce0500
bj.lee <tommy500@sk.com>
2021-04-16 16:22:26 +0900

    Add test case for nugu-login-kit #1743

---
+++ nugu-login-kit/src/test/java/com/skt/nugu/sdk/platform/android/login/auth/CredentialsTest.kt
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2021 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skt.nugu.sdk.platform.android.login.auth
+
+import org.json.JSONException
+import org.junit.Assert
+import org.junit.Test
+import java.util.*
+
+class CredentialsTest {
+    @Test
+    fun `Parse JSON with Credentials`() {
+        val json = Credentials(
+            accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+            refreshToken = "",
+            expiresIn = 10L,
+            issuedTime = Date().time,
+            tokenType = "Bearer",
+            scope = "device:S.I.D."
+        ).toString()
+        Assert.assertTrue(json.isNotEmpty())
+
+        val credentials = Credentials.parse(json)
+        Assert.assertNotNull(credentials)
+        Assert.assertEquals(credentials.accessToken,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9")
+    }
+
+    @Test(expected = JSONException::class)
+    fun `JSONObject(access_token) not found`() {
+        Credentials.parse(
+            "{\n" +
+                    "  \"expires_in\": \"1\",\n" +
+                    "  \"token_type\": \"Bearer\",\n" +
+                    "  \"jti\": \"d1d90f19-3ab5-42e9-ba6d-4c9b74b559db\"\n" +
+                    "}"
+        )
+    }
+
+    @Test
+    fun `JSONObject to string`() {
+        val expected = "{\"access_token\":\"123\",\"refresh_token\":\"321\",\"scope\":\"scope\",\"token_type\":\"type\",\"issued_time\":2,\"expires_in\":1}"
+        val credentials = Credentials(
+            accessToken = "123",
+            refreshToken = "321",
+            expiresIn = 1L,
+            issuedTime = 2L,
+            tokenType = "type",
+            scope = "scope"
+        )
+        Assert.assertEquals(credentials.toString(), expected)
+    }
+}
---
+++ nugu-login-kit/src/test/java/com/skt/nugu/sdk/platform/android/login/auth/NuguOAuthErrorTest.kt
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2021 SK Telecom Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.skt.nugu.sdk.platform.android.login.auth
+
+import android.content.ActivityNotFoundException
+import com.skt.nugu.sdk.platform.android.login.exception.BaseException
+import junit.framework.TestCase
+import org.junit.Assert
+import org.junit.Test
+import java.lang.RuntimeException
+import java.net.UnknownHostException
+
+class NuguOAuthErrorTest : TestCase() {
+    @Test
+    fun testOAuthError() {
+        val oAuthError1 = NuguOAuthError(throwable = ActivityNotFoundException())
+        Assert.assertTrue(oAuthError1.error == NuguOAuthError.ACTIVITY_NOT_FOUND_ERROR)
+        val oAuthError2 = NuguOAuthError(throwable = UnknownHostException())
+        Assert.assertTrue(oAuthError2.error == NuguOAuthError.NETWORK_ERROR)
+        val oAuthError3 = NuguOAuthError(throwable = SecurityException())
+        Assert.assertTrue(oAuthError3.error == NuguOAuthError.SECURITY_ERROR)
+        val oAuthError4 = NuguOAuthError(throwable = UninitializedPropertyAccessException())
+        Assert.assertTrue(oAuthError4.error == NuguOAuthError.INITIALIZE_ERROR)
+        val oAuthError5 = NuguOAuthError(throwable = RuntimeException())
+        Assert.assertTrue(oAuthError5.error == NuguOAuthError.UNKNOWN_ERROR)
+        val oAuthError6 = NuguOAuthError(throwable = BaseException.HttpErrorException(404, "not found"))
+        Assert.assertTrue(oAuthError6.error == NuguOAuthError.NETWORK_ERROR)
+        Assert.assertTrue(oAuthError6.httpCode == 404)
+        Assert.assertTrue(oAuthError6.description == "not found")
+    }
+}
