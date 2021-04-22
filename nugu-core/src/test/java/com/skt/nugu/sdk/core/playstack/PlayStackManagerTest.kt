a8e6301535464c2e43913a214d392f5298c22d61
sangho.lee <sangho0907.lee@sk.com>
2021-04-22 16:42:04 +0900

    Test PlayStackManager #1774

--- nugu-core/src/test/java/com/skt/nugu/sdk/core/playstack/PlayStackManagerTest.kt
+++ nugu-core/src/test/java/com/skt/nugu/sdk/core/playstack/PlayStackManagerTest.kt
@@ -22,7 +22,77 @@ import org.junit.Test
 
 class PlayStackManagerTest {
     @Test
-    fun testGetPlayStack() {
+    fun testAddProvider() {
+        val playStackManager = PlayStackManager("test")
+
+        playStackManager.addPlayContextProvider(object :
+            PlayStackManagerInterface.PlayContextProvider {
+            override fun getPlayContext(): PlayStackManagerInterface.PlayContext =
+                PlayStackManagerInterface.PlayContext("playServiceId_1", 1)
+        })
+
+        val stack = playStackManager.getPlayStack()
+        Assert.assertTrue(stack.size == 1)
+        Assert.assertTrue(stack.first().playServiceId == "playServiceId_1")
+    }
+
+    @Test
+    fun testRemoveProvider() {
+        val playStackManager = PlayStackManager("test")
+
+        val provider = object :
+            PlayStackManagerInterface.PlayContextProvider {
+            override fun getPlayContext(): PlayStackManagerInterface.PlayContext =
+                PlayStackManagerInterface.PlayContext("playServiceId_1", 1)
+        }
+
+        playStackManager.addPlayContextProvider(provider)
+        playStackManager.removePlayContextProvider(provider)
+
+        Assert.assertTrue(playStackManager.getPlayStack().isEmpty())
+    }
+
+    @Test
+    fun testNonPersistentRemoval() {
+        val playStackManager = PlayStackManager("test")
+
+        playStackManager.addPlayContextProvider(object :
+            PlayStackManagerInterface.PlayContextProvider {
+            override fun getPlayContext(): PlayStackManagerInterface.PlayContext =
+                PlayStackManagerInterface.PlayContext("playServiceId_1", 1)
+        })
+        playStackManager.addPlayContextProvider(object :
+            PlayStackManagerInterface.PlayContextProvider {
+            override fun getPlayContext(): PlayStackManagerInterface.PlayContext =
+                PlayStackManagerInterface.PlayContext("playServiceId_2", 2, persistent = false)
+        })
+
+        val stack = playStackManager.getPlayStack()
+        Assert.assertTrue(stack.size == 1)
+        Assert.assertTrue(stack.first().playServiceId == "playServiceId_1")
+    }
+
+    @Test
+    fun testNonAffectPersistent() {
+        val playStackManager = PlayStackManager("test")
+
+        playStackManager.addPlayContextProvider(object :
+            PlayStackManagerInterface.PlayContextProvider {
+            override fun getPlayContext(): PlayStackManagerInterface.PlayContext =
+                PlayStackManagerInterface.PlayContext("playServiceId_1", 1, affectPersistent = false)
+        })
+
+        playStackManager.addPlayContextProvider(object :
+            PlayStackManagerInterface.PlayContextProvider {
+            override fun getPlayContext(): PlayStackManagerInterface.PlayContext =
+                PlayStackManagerInterface.PlayContext("playServiceId_2", 2, affectPersistent = false)
+        })
+
+        Assert.assertTrue(playStackManager.getPlayStack().size == 2)
+    }
+
+    @Test
+    fun testDescendingOrder() {
         val playStackManager = PlayStackManager("test")
 
         for (i in 0..3) {
