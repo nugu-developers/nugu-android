8954184562c6e4b3ef8f84a04099324ab99d7f1c
shshshl09 <53801377+shshshl09@users.noreply.github.com>
2021-05-18 10:49:27 +0900

    Revert "Clear chips at IDLE state #1779"
    This reverts commit 54f3a115fc4ae9040355e229f2dae020d172921a.

--- nugu-agent/src/main/java/com/skt/nugu/sdk/agent/dialog/DialogUXStateAggregator.kt
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/dialog/DialogUXStateAggregator.kt
@@ -207,9 +207,6 @@ class DialogUXStateAggregator(
 
         if(newState == DialogUXStateAggregatorInterface.DialogUXState.IDLE) {
             seamlessFocusManager.cancel(focusRequester)
-            if(currentState != DialogUXStateAggregatorInterface.DialogUXState.IDLE) {
-                lastReceivedChips = null
-            }
         } else {
             if(currentState == DialogUXStateAggregatorInterface.DialogUXState.IDLE) {
                 seamlessFocusManager.prepare(focusRequester)
