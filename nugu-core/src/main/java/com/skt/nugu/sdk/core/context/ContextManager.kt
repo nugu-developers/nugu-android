803e773f8c7def9d6777bc9fb2384a8542544389
sangho.lee <sangho0907.lee@sk.com>
2021-11-22 13:54:09 +0900

    Refactor context manager
    * access cached state with ContextType
    * log the time taken which to get state

--- nugu-core/src/main/java/com/skt/nugu/sdk/core/context/ContextManager.kt
+++ nugu-core/src/main/java/com/skt/nugu/sdk/core/context/ContextManager.kt
@@ -19,10 +19,12 @@ package com.skt.nugu.sdk.core.context
 import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
 import com.skt.nugu.sdk.core.interfaces.context.*
 import com.skt.nugu.sdk.core.utils.Logger
+import java.util.*
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReentrantLock
+import kotlin.collections.HashMap
 import kotlin.concurrent.withLock
 
 class ContextManager : ContextManagerInterface {
@@ -40,14 +42,16 @@ class ContextManager : ContextManagerInterface {
         var outdateFuture: ScheduledFuture<*>? = null,
         val pendings: MutableMap<NamespaceAndName, ContextType> = HashMap(),
 
+        // for debug
+        val requestedTimes: MutableMap<NamespaceAndName, Long> = HashMap(),
+
         // output
         val updated: MutableMap<NamespaceAndName, SetStateParam> = HashMap()
     )
 
     private data class StateInfo(
         var refreshPolicy: StateRefreshPolicy,
-        var full: CachedStateContext?,
-        var compact: CachedStateContext?
+        val state: MutableMap<ContextType, CachedStateContext> = EnumMap(ContextType::class.java)
     )
 
     private data class CachedStateContext(val delegate: BaseContextState) : BaseContextState {
@@ -106,21 +110,25 @@ class ContextManager : ContextManagerInterface {
             }
 
             // TODO type check
+            if(pendingType != type) {
+                Logger.w(TAG, "[setState] InvalidType - request type: $pendingType, set type: $type")
+            }
 
             updateStateLocked(namespaceAndName, state, refreshPolicy, type)
             request.updated[namespaceAndName] = SetStateParam(state, refreshPolicy, type)
 
-            if(type == ContextType.FULL) {
-                Logger.d(
+            val timeTaken = System.currentTimeMillis() - (request.requestedTimes[namespaceAndName] ?: 0)
+            val updatedState: CachedStateContext? = namespaceNameToStateInfo[namespaceAndName]?.state?.get(type)
+
+            Logger.d(
                     TAG,
-                    "[setState] success - namespaceAndName: $namespaceAndName, state: ${namespaceNameToStateInfo[namespaceAndName]?.full}, refreshPolicy: $refreshPolicy, type: $type, stateRequestToken: $stateRequestToken"
-                )
-            } else {
-                Logger.d(
-                    TAG,
-                    "[setState] success - namespaceAndName: $namespaceAndName, state: ${namespaceNameToStateInfo[namespaceAndName]?.compact}, refreshPolicy: $refreshPolicy, type: $type, stateRequestToken: $stateRequestToken"
-                )
-            }
+                    "[setState] success - namespaceAndName: $namespaceAndName, " +
+                            "state: $updatedState, " +
+                            "refreshPolicy: $refreshPolicy, " +
+                            "type: $type, " +
+                            "stateRequestToken: $stateRequestToken, " +
+                            "(takes time to collect state: ${timeTaken}ms)"
+            )
 
             if (request.pendings.isEmpty()) {
                 request.outdateFuture?.cancel(true)
@@ -149,19 +157,15 @@ class ContextManager : ContextManagerInterface {
 
         var stateInfo = namespaceNameToStateInfo[namespaceAndName]
         if(stateInfo == null) {
-            stateInfo = if(type == ContextType.FULL) {
-                StateInfo(refreshPolicy, CachedStateContext(state), null)
-            } else {
-                StateInfo(refreshPolicy, null, CachedStateContext(state))
+            stateInfo = StateInfo(refreshPolicy).apply {
+                this.state[type] = CachedStateContext(state)
             }
             namespaceNameToStateInfo[namespaceAndName] = stateInfo
         } else {
             if(type == ContextType.FULL) {
-                stateInfo.full = CachedStateContext(state)
                 stateInfo.refreshPolicy = refreshPolicy
-            } else {
-                stateInfo.compact = CachedStateContext(state)
             }
+            stateInfo.state[type] = CachedStateContext(state)
         }
 
         var nameAndStateInfo = namespaceToNameStateInfo[namespaceAndName.namespace]
@@ -210,11 +214,9 @@ class ContextManager : ContextManagerInterface {
                         if(stateInfo == null) {
                             pendings[it.key] = requiredContextType
                         } else {
-                            if(requiredContextType == ContextType.COMPACT && stateInfo.compact == null) {
+                            if(stateInfo.state[requiredContextType] == null) {
                                 pendings[it.key] = requiredContextType
-                            } else if(requiredContextType == ContextType.FULL && stateInfo.full == null) {
-                                pendings[it.key] = requiredContextType
-                            } else if(stateInfo.refreshPolicy != StateRefreshPolicy.NEVER){
+                            } else if(stateInfo.refreshPolicy != StateRefreshPolicy.NEVER) {
                                 pendings[it.key] = requiredContextType
                             }
                         }
@@ -235,6 +237,7 @@ class ContextManager : ContextManagerInterface {
                 HashMap(pendings).forEach {
                     stateProviders[it.key]?.let { provider->
                         provideStateExecutor.submit {
+                            requestedTimes[it.key] = System.currentTimeMillis()
                             provider.provideState(
                                 this@ContextManager,
                                 it.key,
@@ -282,8 +285,8 @@ class ContextManager : ContextManagerInterface {
             append('{')
             var valueIndex = 0
             namespaceEntry.value.forEach {
-                val fullState = it.value.full?.value()
-                val compactState = it.value.compact?.value()
+                val fullState = it.value.state[ContextType.FULL]?.value()
+                val compactState = it.value.state[ContextType.COMPACT]?.value()
                 val givenContextState = given?.get(NamespaceAndName(namespaceEntry.key, it.key))
                 val value: String? = if (target == null || (target.namespace == namespaceEntry.key && target.name == it.key) || compactState == null) {
                     // need full
