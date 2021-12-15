b3ada76fc57b66750f00b2c41266ab54fb5b2458
sangho.lee <sangho0907.lee@sk.com>
2021-12-15 16:00:37 +0900

    Set attribute to DialogAttributeStorage #2175

--- nugu-agent/src/main/java/com/skt/nugu/sdk/agent/text/ExpectTypingDirectiveHandler.kt
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/text/ExpectTypingDirectiveHandler.kt
@@ -3,14 +3,21 @@ package com.skt.nugu.sdk.agent.text
 import com.skt.nugu.sdk.agent.AbstractDirectiveHandler
 import com.skt.nugu.sdk.agent.util.MessageFactory
 import com.skt.nugu.sdk.core.interfaces.common.NamespaceAndName
+import com.skt.nugu.sdk.core.interfaces.dialog.DialogAttributeStorageInterface
 import com.skt.nugu.sdk.core.interfaces.directive.BlockingPolicy
+import com.skt.nugu.sdk.core.interfaces.display.InterLayerDisplayPolicyManager
+import com.skt.nugu.sdk.core.interfaces.message.Header
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.collections.HashMap
+import kotlin.collections.LinkedHashMap
+import kotlin.concurrent.withLock
 
 internal class ExpectTypingDirectiveHandler(
-    private val textAttributeStorage: TextAttributeStorage,
+    private val dialogAttributeStorage: DialogAttributeStorageInterface,
     private val controller: ExpectTypingHandlerInterface.Controller
-    ) :
-    AbstractDirectiveHandler(), ExpectTypingHandlerInterface {
+    ) : AbstractDirectiveHandler(), ExpectTypingHandlerInterface, InterLayerDisplayPolicyManager.Listener {
     companion object {
         private const val NAME_EXPECT_TYPING = "ExpectTyping"
 
@@ -22,6 +29,11 @@ internal class ExpectTypingDirectiveHandler(
 
     private val directives = ConcurrentHashMap<String, ExpectTypingHandlerInterface.Directive>()
 
+    private val lock = ReentrantLock()
+
+    private val pendingAttributes = LinkedHashMap<Header, DialogAttributeStorageInterface.Attribute>()
+    private val activatedTextAttributeKeys = Stack<Header>()
+
     override fun preHandleDirective(info: DirectiveInfo) {
         val payload = MessageFactory.create(info.directive.payload, ExpectTypingHandlerInterface.Payload::class.java)
 
@@ -30,14 +42,10 @@ internal class ExpectTypingDirectiveHandler(
             return
         }
 
-        textAttributeStorage.setAttributes(
-            info.directive.header.dialogRequestId,
-            HashMap<String, Any>().apply {
-                put("playServiceId", payload.playServiceId)
-                payload.domainTypes?.let {
-                    put("domainTypes", it)
-                }
-            })
+        lock.withLock {
+            pendingAttributes[info.directive.header] =
+                ExpectTypingHandlerInterface.Payload.getDialogAttribute(payload)
+        }
 
         directives[info.directive.header.messageId] = ExpectTypingHandlerInterface.Directive(info.directive.header, payload)
     }
@@ -46,7 +54,9 @@ internal class ExpectTypingDirectiveHandler(
         val directive = directives.remove(info.directive.header.messageId)
 
         if (directive == null) {
-            textAttributeStorage.removeAttributes(info.directive.header.dialogRequestId)
+            lock.withLock {
+                pendingAttributes.remove(info.directive.header)
+            }
             info.result.setFailed("canceled: ${info.directive.header}")
             return
         }
@@ -56,12 +66,39 @@ internal class ExpectTypingDirectiveHandler(
     }
 
     override fun cancelDirective(info: DirectiveInfo) {
-        textAttributeStorage.removeAttributes(info.directive.header.dialogRequestId)
+        lock.withLock {
+            pendingAttributes.remove(info.directive.header)
+        }
         directives.remove(info.directive.header.messageId)
     }
 
     override val configurations: Map<NamespaceAndName, BlockingPolicy> = HashMap<NamespaceAndName, BlockingPolicy>().apply {
         // only blocked by audio
         this[EXPECT_TYPING] = BlockingPolicy(BlockingPolicy.MEDIUM_AUDIO)
+    }
+
+    override fun onDisplayLayerRendered(layer: InterLayerDisplayPolicyManager.DisplayLayer) {
+        lock.withLock {
+            pendingAttributes.firstNotNullOfOrNull {
+                if(it.key.dialogRequestId == layer.getDialogRequestId()) {
+                    it
+                } else {
+                    null
+                }
+            }?.let {
+                pendingAttributes.remove(it.key)
+                activatedTextAttributeKeys.push(it.key)
+                dialogAttributeStorage.setAttribute(it.key.messageId, it.value)
+            }
+        }
+    }
+
+    override fun onDisplayLayerCleared(layer: InterLayerDisplayPolicyManager.DisplayLayer) {
+        lock.withLock {
+            activatedTextAttributeKeys.find { it.dialogRequestId == layer.getDialogRequestId() }?.let {
+                activatedTextAttributeKeys.remove(it)
+                dialogAttributeStorage.removeAttribute(it.messageId)
+            }
+        }
     }
 }
