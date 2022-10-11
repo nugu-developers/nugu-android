604996d858ab722b37b54f879ad55128a343fbc7
sangho.lee <sangho0907.lee@sk.com>
2022-10-11 22:34:48 +0900

    Test log

--- nugu-agent/src/main/java/com/skt/nugu/sdk/agent/routine/RoutineAgent.kt
+++ nugu-agent/src/main/java/com/skt/nugu/sdk/agent/routine/RoutineAgent.kt
@@ -309,7 +309,8 @@ class RoutineAgent(
             val directiveGroupPrepareListener = object : DirectiveGroupHandlingListener.OnDirectiveGroupPrepareListener {
                 override fun onPrepared(directives: List<Directive>) {
                     Logger.d(TAG, "[onPrepared] action index: $currentActionIndex, ${directives.firstOrNull()?.getDialogRequestId()}")
-                    if(action.muteDelayInMilliseconds != null && !directives.any { it.header.namespace == "TTS" && it.header.name == "SPEAK" }) {
+                    if(action.muteDelayInMilliseconds != null && !directives.any { it.header.namespace == "TTS" && it.header.name == "Speak" }) {
+                        Logger.d(TAG, "[onPrepared] apply mute delay, $directives")
                         applyMuteDelay = true
                     }
                     action.actionTimeoutInMilliseconds?.let {
@@ -338,9 +339,13 @@ class RoutineAgent(
                         } else {
                             cancelNextScheduledAction()
                             val delay =
-                                if (action.muteDelayInMilliseconds != null && applyMuteDelay) {
+                                if (applyMuteDelay) {
+                                    Logger.d(TAG, "[onFinish] apply mute delay: ${action.muteDelayInMilliseconds}")
                                     action.muteDelayInMilliseconds
-                                } else action.postDelayInMilliseconds
+                                } else {
+                                    Logger.d(TAG, "[onFinish] apply post delay: ${action.postDelayInMilliseconds}")
+                                    action.postDelayInMilliseconds
+                                }
 
                             if (delay != null) {
                                 scheduledFutureForTryStartNextAction =
