4a1989d5bd17b420629511f312e173c2b4606153
sangho.lee <sangho0907.lee@sk.com>
2024-06-17 16:18:29 +0900

    Asr's response asynckey handling

--- nugu-core/src/main/java/com/skt/nugu/sdk/core/inputprocessor/InputProcessorManager.kt
+++ nugu-core/src/main/java/com/skt/nugu/sdk/core/inputprocessor/InputProcessorManager.kt
@@ -15,9 +15,12 @@
  */
 package com.skt.nugu.sdk.core.inputprocessor
 
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import com.skt.nugu.sdk.core.interfaces.directive.DirectiveGroupProcessorInterface
 import com.skt.nugu.sdk.core.interfaces.inputprocessor.InputProcessor
 import com.skt.nugu.sdk.core.interfaces.inputprocessor.InputProcessorManagerInterface
+import com.skt.nugu.sdk.core.interfaces.message.AsyncKey
 import com.skt.nugu.sdk.core.interfaces.message.Directive
 import com.skt.nugu.sdk.core.utils.Logger
 import java.util.concurrent.*
@@ -27,15 +30,36 @@ class InputProcessorManager(private val timeoutInMilliSeconds: Long = 10 * 1000L
         private const val TAG = "InputProcessorManager"
     }
 
+    private data class AsyncKeyPayload(
+        @SerializedName("asyncKey")
+        val asyncKey: AsyncKey
+    )
+
     private val responseTimeoutListeners = HashSet<InputProcessorManagerInterface.OnResponseTimeoutListener>()
     private val requests = ConcurrentHashMap<String, InputProcessor>()
     private val timeoutFutureMap = ConcurrentHashMap<String, ScheduledFuture<*>>()
     private val timeoutScheduler = Executors.newSingleThreadScheduledExecutor()
 
     override fun onPostProcessed(directives: List<Directive>) {
-        val dialogRequestId = directives.firstOrNull()?.header?.dialogRequestId ?: return
+        val sampleDirective = directives.firstOrNull()
 
-        val receiveResponse = requests[dialogRequestId]?.onReceiveDirectives(dialogRequestId, directives) ?: false
+        val directiveDialogRequestId = sampleDirective?.header?.dialogRequestId ?: return
+        val asyncKey = getAsyncKey(sampleDirective)
+        val asyncKeyEventDialogRequestId = asyncKey?.eventDialogRequestId
+
+        val dialogRequestId = if(requests.contains(directiveDialogRequestId)) {
+            directiveDialogRequestId
+        } else if(asyncKeyEventDialogRequestId != null && requests.contains(asyncKeyEventDialogRequestId)) {
+            asyncKeyEventDialogRequestId
+        } else {
+            null
+        }
+
+        if(dialogRequestId == null) {
+            return
+        }
+
+        val receiveResponse = requests[dialogRequestId]?.onReceiveDirectives(dialogRequestId, directives, asyncKey) ?: false
 
         if(receiveResponse) {
             dialogRequestId.let {
@@ -44,10 +68,20 @@ class InputProcessorManager(private val timeoutInMilliSeconds: Long = 10 * 1000L
             }
         }
 
-        if(receiveResponse && !dialogRequestId.isBlank()) {
+        if(receiveResponse && dialogRequestId.isNotBlank()) {
             requests.remove(dialogRequestId)
         }
     }
+
+    private fun getAsyncKey(directive: Directive): AsyncKey? =
+        runCatching {
+            if (directive.payload.contains("\"asyncKey\"")) {
+                Gson().fromJson(
+                    directive.payload,
+                    AsyncKeyPayload::class.java
+                ).asyncKey
+            } else null
+        }.getOrNull()
 
     override fun onRequested(inputProcessor: InputProcessor, dialogRequestId: String) {
         Logger.d(TAG, "[onRequested] $inputProcessor, $dialogRequestId")
