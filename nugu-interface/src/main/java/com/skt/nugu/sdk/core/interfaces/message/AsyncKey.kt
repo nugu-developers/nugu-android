80e7afb6b0ba9565fe94263ddf8e072d18b4bfd6
sangho.lee <sangho0907.lee@sk.com>
2024-06-28 09:53:02 +0900

    test asynkey state

--- nugu-interface/src/main/java/com/skt/nugu/sdk/core/interfaces/message/AsyncKey.kt
+++ nugu-interface/src/main/java/com/skt/nugu/sdk/core/interfaces/message/AsyncKey.kt
@@ -2,7 +2,7 @@ package com.skt.nugu.sdk.core.interfaces.message
 
 data class AsyncKey(
     val eventDialogRequestId: String,
-    val state: State
+    val state: State?
 ) {
     enum class State {
         START, ONGOING, END
